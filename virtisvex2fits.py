128af92f31a1882d4f8cf7048aee40812d841676
Chiara Marmo <chiara.marmo@u-psud.fr>
2018-06-08 14:16:19 +0200

    Cleaning script

--- virtisvex2fits.py
+++ virtisvex2fits.py
@@ -5,9 +5,10 @@ import numpy as np
 from astropy.io import fits
 from struct import *
 
-myversion = 1.0
+myversion = 1.1
 
 # HISTORY
+# 8/6/2018 It works with raw data: cubes are just translated in FITS 
 # 22/3/2018 first version 
 
 i = 0
@@ -16,8 +17,8 @@ for arg in sys.argv:
     if (arg[0] != "-"):
       myimage = arg
 
-      myfits = myimage.replace("CAL","fits")
-      mygeom = myimage.replace("CAL","GEO")
+      myfits = myimage.replace("QUB","fits")
+      mygeom = myimage.replace("QUB","GEO")
 
 
       if os.path.exists(myimage):
@@ -115,12 +116,6 @@ for arg in sys.argv:
           for x in range(0, mysamples):
             contents=myfile.read(bytex)
             bsq[y,x,:]=unpack_from(form, contents)
-            #if (z<IRbands):
-            #  bsqIR1[z,y,:]=unpack_from(form, contents)
-            #elif (z<2*IRbands):
-            #  bsqIR2[z-IRbands,y,:]=unpack_from(form, contents)
-            #else:
-            #  bsqV[z-(2*IRbands),y,:]=unpack_from(form, contents)
             offset=myfile.tell()+ (sbands*sbytes)
             myfile.seek(offset)
 
@@ -128,6 +123,8 @@ for arg in sys.argv:
           myfile.seek(offset)
 
         myfile.close()
+
+        ### writing FITS ###
         hdu = fits.PrimaryHDU(bsq)
         hdr = hdu.header
         hdr['TELESCOP'] = telescop
@@ -141,201 +138,3 @@ for arg in sys.argv:
         exit(1)
 
   i = i+1
-"""
-        if os.path.exists(mygeom):
-          toparse = mygeom
-          ### parsing PDS label from geometry cube ### 
-          myfile = open(toparse,'r')
-          myskip = 0
-          rbytes = 1
-          lastlabrec = 0
-          myarch = 'littleendian'
-          lablines = 0
-          for line in myfile:
-            lablines = lablines + 1
-            if (line == "END\r\n" or line == "END\n"):
-              break
-            temp = line.split('=')
-            if (temp[0].strip(' ') == "RECORD_BYTES"):
-              rbytes = int(temp[1].strip(' '))
-            if (temp[0].strip(' ') == "^QUBE"):
-              mystr = temp[1].strip(' ')
-              tests = re.match('["a-zA-Z]',mystr)
-              if (tests == None):
-                lastlabrec = int(mystr.strip('\r\n')) - 1
-            if (temp[0].strip(' ') == "CORE_ITEM_BYTES"):
-              imbytes = int(temp[1].strip(' '))
-
-          ### Reading coordinates from geometry cube ###
-          if ((rbytes != 0) and (lastlabrec != 0)):
-            myskip = rbytes * lastlabrec
-          myfile.seek(myskip)
-          dim = (mylines,mysamples,2)
-          coordsIR1 = np.zeros(dim,dtype=np.int32)
-          incIR1 = np.zeros((mylines,mysamples),dtype=np.int32)
-          emIR1 = np.zeros((mylines,mysamples),dtype=np.int32)
-          paIR1 = np.zeros((mylines,mysamples),dtype=np.int32)
-          coordsIR2 = np.zeros(dim,dtype=np.int32)
-          incIR2 = np.zeros((mylines,mysamples),dtype=np.int32)
-          emIR2 = np.zeros((mylines,mysamples),dtype=np.int32)
-          paIR2 = np.zeros((mylines,mysamples),dtype=np.int32)
-          coordsV = np.zeros(dim,dtype=np.int32)
-          incV = np.zeros((mylines,mysamples),dtype=np.int32)
-          emV = np.zeros((mylines,mysamples),dtype=np.int32)
-          paV = np.zeros((mylines,mysamples),dtype=np.int32)
-          bytex = mysamples * imbytes
-          form = str(mysamples) + 'i'
-          for y in range(0, mylines):
-            offset=myfile.tell()+(6*bytex)
-            myfile.seek(offset)
-            contents=myfile.read(bytex)
-            coordsIR1[y,:,0]=unpack_from(form, contents)
-            contents=myfile.read(bytex)
-            coordsIR1[y,:,1]=unpack_from(form, contents)
-            contents=myfile.read(bytex)
-            incIR1[y,:]=unpack_from(form, contents)
-            contents=myfile.read(bytex)
-            emIR1[y,:]=unpack_from(form, contents)
-            contents=myfile.read(bytex)
-            paIR1[y,:]=unpack_from(form, contents)
-            offset=myfile.tell()+(10*bytex)
-            myfile.seek(offset)
-            contents=myfile.read(bytex)
-            coordsIR2[y,:,0]=unpack_from(form, contents)
-            contents=myfile.read(bytex)
-            coordsIR2[y,:,1]=unpack_from(form, contents)
-            contents=myfile.read(bytex)
-            incIR2[y,:]=unpack_from(form, contents)
-            contents=myfile.read(bytex)
-            emIR2[y,:]=unpack_from(form, contents)
-            contents=myfile.read(bytex)
-            paIR2[y,:]=unpack_from(form, contents)
-            offset=myfile.tell()+(10*bytex)
-            myfile.seek(offset)
-            contents=myfile.read(bytex)
-            coordsV[y,:,0]=unpack_from(form, contents)
-            contents=myfile.read(bytex)
-            coordsV[y,:,1]=unpack_from(form, contents)
-            contents=myfile.read(bytex)
-            incV[y,:]=unpack_from(form, contents)
-            contents=myfile.read(bytex)
-            emV[y,:]=unpack_from(form, contents)
-            contents=myfile.read(bytex)
-            paV[y,:]=unpack_from(form, contents)
-            offset=myfile.tell()+(10*bytex)
-            myfile.seek(offset)
-          factor = 0.0001
-        else:
-          print 'Cannot find geometry for image %s!\n' %(myimage)
-          continue
-"""
-
-        ### Writing FITS files ###
-
-"""
-        ### TAB formatting ###
-        form = str(2*mysamples*mylines)
-        ttype = "COORDS"
-        tform= form+'J';
-        tunit = 'deg';
-        tdim = '(2,' + str(mysamples) + ',' + str(mylines) + ')'
-
-        ### WCS-TAB header info ###
-        head = fits.Header()
-        head.set('ctype1', 'MALN-TAB')
-        head.set('ctype2', 'MALT-TAB')
-        head.set('CRPIX1', 1.0)
-        head.set('CRPIX2', 1.0)
-        head.set('CRVAL1', 1.0)
-        head.set('CRVAL2', 1.0)
-        head.set('CD1_1', 1.0)
-        head.set('CD2_2', 1.0)
-        head.set('CD1_2', 0.0)
-        head.set('CD2_1', 0.0)
-        head.set('PC1_1', 1.0)
-        head.set('PC2_2', 1.0)
-        head.set('PC1_2', 0.0)
-        head.set('PC2_1', 0.0)
-        head.set('CDELT1', 1.0)
-        head.set('CDELT2', 1.0)
-        head.set('PS1_0 ', 'WCS-TAB ')
-        head.set('PS1_1 ', 'COORDS ')
-        head.set('PS2_0 ', 'WCS-TAB ')
-        head.set('PS2_1 ', 'COORDS ')
-        head.set('PV1_1 ', 1)
-        head.set('PV2_1 ', 1)
-        head.set('PV1_2 ', 1)
-        head.set('PV2_2 ', 1)
-        head.set('PV1_3 ', 1)
-        head.set('PV2_3 ', 2)
-
-        # SWIR-C channel #
-        hduIR1 = fits.PrimaryHDU(bsqIR1)
-        tbhduIR1 = fits.BinTableHDU.from_columns([fits.Column(name=ttype, unit=tunit, dim=tdim, format=tform, array=[coordsIR1])])
-        hduincIR1 = fits.ImageHDU(incIR1)
-        hduemIR1 = fits.ImageHDU(emIR1)
-        hdupaIR1 = fits.ImageHDU(paIR1)
-        listIR1 = fits.HDUList([hduIR1,tbhduIR1,hduincIR1,hduemIR1,hdupaIR1])
-        IR1hdr = listIR1[0].header
-        IR1hdr += head
-        IR1thdr = listIR1[1].header
-        IR1thdr.set('extname', 'WCS-TAB')
-        IR1thdr.set('TSCAL1', factor)
-        IR1ihdr = listIR1[2].header
-        IR1ihdr.set('extname', 'INCIDENCE')
-        IR1ihdr.set('BSCALE', factor)
-        IR1ehdr = listIR1[3].header
-        IR1ehdr.set('extname', 'EMISSION')
-        IR1ehdr.set('BSCALE', factor)
-        IR1pahdr = listIR1[4].header
-        IR1pahdr.set('extname', 'PHASE-ANGLE')
-        IR1pahdr.set('BSCALE', factor)
-        listIR1.writeto(myfitsIR1)
-
-        # SWIR-L channel #
-        hduIR2 = fits.PrimaryHDU(bsqIR2)
-        tbhduIR2 = fits.BinTableHDU.from_columns([fits.Column(name=ttype, unit=tunit, dim=tdim, format=tform, array=[coordsIR2])])
-        hduincIR2 = fits.ImageHDU(incIR2)
-        hduemIR2 = fits.ImageHDU(emIR2)
-        hdupaIR2 = fits.ImageHDU(paIR2)
-        listIR2 = fits.HDUList([hduIR2,tbhduIR2,hduincIR2,hduemIR2,hdupaIR2])
-        IR2hdr = listIR2[0].header
-        IR2hdr += head
-        IR2thdr = listIR2[1].header
-        IR2thdr.set('extname', 'WCS-TAB')
-        IR2thdr.set('TSCAL1', factor)
-        IR2ihdr = listIR2[2].header
-        IR2ihdr.set('extname', 'INCIDENCE')
-        IR2ihdr.set('BSCALE', factor)
-        IR2ehdr = listIR2[3].header
-        IR2ehdr.set('extname', 'EMISSION')
-        IR2ehdr.set('BSCALE', factor)
-        IR2pahdr = listIR2[4].header
-        IR2pahdr.set('extname', 'PHASE-ANGLE')
-        IR2pahdr.set('BSCALE', factor)
-        listIR2.writeto(myfitsIR2)
-
-        # VNIR channel #
-        hduV = fits.PrimaryHDU(bsqV)
-        tbhduV = fits.BinTableHDU.from_columns([fits.Column(name=ttype, unit=tunit, dim=tdim, format=tform, array=[coordsV])])
-        hduincV = fits.ImageHDU(incV)
-        hduemV = fits.ImageHDU(emV)
-        hdupaV = fits.ImageHDU(paV)
-        listV = fits.HDUList([hduV,tbhduV,hduincV,hduemV,hdupaV])
-        Vhdr = listV[0].header
-        Vhdr += head
-        Vthdr = listV[1].header
-        Vthdr.set('extname', 'WCS-TAB')
-        Vthdr.set('TSCAL1', factor)
-        Vihdr = listV[2].header
-        Vihdr.set('extname', 'INCIDENCE')
-        Vihdr.set('BSCALE', factor)
-        Vehdr = listV[3].header
-        Vehdr.set('extname', 'EMISSION')
-        Vehdr.set('BSCALE', factor)
-        Vpahdr = listV[4].header
-        Vpahdr.set('extname', 'PHASE-ANGLE')
-        Vpahdr.set('BSCALE', factor)
-        listV.writeto(myfitsV)
-"""
-
