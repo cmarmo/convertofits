bd512e756dd49574258c5f970c1aecc2592795ee
Chiara Marmo <chiara.marmo@u-psud.fr>
2018-06-27 16:28:42 +0200

    Adding TAB coordinates and extensions

--- imvvex2fits.py
+++ imvvex2fits.py
@@ -106,7 +106,6 @@ for arg in sys.argv:
             mytype = np.float64
             myform = "d"
         form = myarch + str(mysamples) + myform
-        print(form)
         if ((rbytes != 0) and (lastlabrec != 0)):
           myskip = rbytes * lastlabrec
         myfile.seek(myskip)
@@ -129,23 +128,205 @@ for arg in sys.argv:
 
         myfile.close()
 
+        if os.path.exists(mygeom):
+          toparse = mygeom
+          ### parsing PDS label from geometry cube ### 
+          myfile = open(toparse,'r')
+          myskip = 0
+          rbytes = 1
+          lastlabrec = 0
+          myarch = '>'
+          lablines = 0
+          for line in myfile:
+            lablines = lablines + 1
+            if (line == "END\r\n" or line == "END\n"):
+              break
+            temp = line.split('=')
+            if (temp[0].strip(' ') == "RECORD_BYTES"):
+              rbytes = int(temp[1].strip(' '))
+            if (temp[0].strip(' ') == "^QUBE"):
+              mystr = temp[1].strip(' ')
+              tests = re.match('["a-zA-Z]',mystr)
+              if (tests == None):
+                lastlabrec = int(mystr.strip('\r\n')) - 1
+            if (temp[0].strip(' ') == "CORE_ITEMS"):
+              mydim = (temp[1].strip(' \r\n')).split(',')
+              mybands = int(re.sub(r'[^\w]', ' ', mydim[0]))
+              mysamples = int(re.sub(r'[^\w]', ' ', mydim[1]))
+              mylines = int(re.sub(r'[^\w]', ' ', mydim[2]))
+            if (temp[0].strip(' ') == "CORE_ITEM_TYPE"):
+              tbyte = (temp[1].strip(' ')).split('_')
+              if (tbyte[0] == "LSB" or tbyte[0] == "PC" or tbyte[0] == "VAX"):
+                  myarch = '<'
+              if (tbyte[0] == "MSB" or tbyte[0] == "MAC" or tbyte[0] == "SUN"):
+                  myarch = '>'
+            if (temp[0].strip(' ') == "CORE_ITEM_BYTES"):
+              imbytes = int(temp[1].strip(' '))
+
+          ### Reading coordinates from geometry cube ###
+          if ((rbytes != 0) and (lastlabrec != 0)):
+            myskip = rbytes * lastlabrec
+          myfile.seek(myskip)
+          dim = (mylines,mysamples,2)
+          data = np.zeros((mybands,mylines,mysamples),dtype=np.int32)
+          coords = np.zeros(dim,dtype=np.int32)
+          coordsa = np.zeros(dim,dtype=np.int32)
+          coordsb = np.zeros(dim,dtype=np.int32)
+          inc = np.zeros((mylines,mysamples),dtype=np.int32)
+          em = np.zeros((mylines,mysamples),dtype=np.int32)
+          pa = np.zeros((mylines,mysamples),dtype=np.int32)
+          bytex = mybands * imbytes
+          form = myarch + str(mybands) + 'i'
+          for x in range(0, mysamples):
+            for y in range(0, mylines):
+              contents=myfile.read(bytex)
+              data[:,y,x] = unpack_from(form, contents)
+          coords[:,:,0] = data[9,:,:]
+          coords[:,:,1] = data[10,:,:]
+          coordsa[:,:,0] = data[25,:,:]
+          coordsa[:,:,1] = data[26,:,:]
+          inc[:,:] = data[27,:,:]
+          em[:,:] = data[28,:,:]
+          pa[:,:] = data[29,:,:]
+          coordsb[:,:,0] = data[31,:,:]
+          coordsb[:,:,1] = data[32,:,:]
+          factor = 0.0001
+        else:
+          print 'Cannot find geometry for image %s!\n' %(myimage)
+          continue
+
         ### writing FITS ###
+
+        ### TAB formatting ###
+        form = str(2*mysamples*mylines)
+        ttype = "COORDS"
+        tform= form+'J';
+        tunit = 'deg';
+        tdim = '(2,' + str(mysamples) + ',' + str(mylines) + ')'
+
+        ### WCS-TAB header info ###
+        head = fits.Header()
+
+        ### Lat Long IAU2000
+        head.set('wcsname', 'Body-fixed planetocentric')
+        head.set('ctype1', 'VELN-TAB')
+        head.set('ctype2', 'VELT-TAB')
+        head.set('CRPIX1', 1.0)
+        head.set('CRPIX2', 1.0)
+        head.set('CRVAL1', 1.0)
+        head.set('CRVAL2', 1.0)
+        head.set('CD1_1', 1.0)
+        head.set('CD2_2', 1.0)
+        head.set('CD1_2', 0.0)
+        head.set('CD2_1', 0.0)
+        head.set('PC1_1', 1.0)
+        head.set('PC2_2', 1.0)
+        head.set('PC1_2', 0.0)
+        head.set('PC2_1', 0.0)
+        head.set('CDELT1', 1.0)
+        head.set('CDELT2', 1.0)
+        head.set('PS1_0 ', 'WCS-TAB ')
+        head.set('PS1_1 ', 'COORDS ')
+        head.set('PS2_0 ', 'WCS-TAB ')
+        head.set('PS2_1 ', 'COORDS ')
+        head.set('PV1_1 ', 1)
+        head.set('PV2_1 ', 1)
+        head.set('PV1_2 ', 1)
+        head.set('PV2_2 ', 1)
+        head.set('PV1_3 ', 1)
+        head.set('PV2_3 ', 2)
+
+        ### Lat Long on 60Km cloud layer reference
+        head.set('wcsnamea', '60Km Venus cloud layer reference')
+        head.set('ctype1a', 'VELN-TAB')
+        head.set('ctype2a', 'VELT-TAB')
+        head.set('CRPIX1a', 1.0)
+        head.set('CRPIX2a', 1.0)
+        head.set('CRVAL1a', 1.0)
+        head.set('CRVAL2a', 1.0)
+        head.set('CD1_1a', 1.0)
+        head.set('CD2_2a', 1.0)
+        head.set('CD1_2a', 0.0)
+        head.set('CD2_1a', 0.0)
+        head.set('PC1_1a', 1.0)
+        head.set('PC2_2a', 1.0)
+        head.set('PC1_2a', 0.0)
+        head.set('PC2_1a', 0.0)
+        head.set('CDELT1a', 1.0)
+        head.set('CDELT2a', 1.0)
+        head.set('PS1_0a ', 'WCS-CTAB')
+        head.set('PS1_1a ', 'COORDS ')
+        head.set('PS2_0a ', 'WCS-CTAB')
+        head.set('PS2_1a ', 'COORDS ')
+        head.set('PV1_1a ', 1)
+        head.set('PV2_1a ', 1)
+        head.set('PV1_2a ', 1)
+        head.set('PV2_2a ', 1)
+        head.set('PV1_3a ', 1)
+        head.set('PV2_3a ', 2)
+
+        ### RA DEC J2000
+        head.set('wcsnameb', 'RA DEC J2000')
+        head.set('ctype1b', 'RA---TAB')
+        head.set('ctype2b', 'DEC--TAB')
+        head.set('CRPIX1b', 1.0)
+        head.set('CRPIX2b', 1.0)
+        head.set('CRVAL1b', 1.0)
+        head.set('CRVAL2b', 1.0)
+        head.set('CD1_1b', 1.0)
+        head.set('CD2_2b', 1.0)
+        head.set('CD1_2b', 0.0)
+        head.set('CD2_1b', 0.0)
+        head.set('PC1_1b', 1.0)
+        head.set('PC2_2b', 1.0)
+        head.set('PC1_2b', 0.0)
+        head.set('PC2_1b', 0.0)
+        head.set('CDELT1b', 1.0)
+        head.set('CDELT2b', 1.0)
+        head.set('PS1_0b ', 'WCS-WTAB')
+        head.set('PS1_1b ', 'COORDS ')
+        head.set('PS2_0b ', 'WCS-WTAB')
+        head.set('PS2_1b ', 'COORDS ')
+        head.set('PV1_1b ', 1)
+        head.set('PV2_1b ', 1)
+        head.set('PV1_2b ', 1)
+        head.set('PV2_2b ', 1)
+        head.set('PV1_3b ', 1)
+        head.set('PV2_3b ', 2)
+
         hdu = fits.PrimaryHDU(bsq)
-        try:
-          hdudark = fits.ImageHDU(bsdark)
-          listfits = fits.HDUList([hdu,hdudark])
-          hdrdark = hdudark.header
-          hdrdark['EXTNAME'] = 'DARK'
-        except:
-          hdr = hdu.header
+        tbhdu = fits.BinTableHDU.from_columns([fits.Column(name=ttype, unit=tunit, dim=tdim, format=tform, array=[coords])])
+        tbahdu = fits.BinTableHDU.from_columns([fits.Column(name=ttype, unit=tunit, dim=tdim, format=tform, array=[coordsa])])
+        tbbhdu = fits.BinTableHDU.from_columns([fits.Column(name=ttype, unit=tunit, dim=tdim, format=tform, array=[coordsb])])
+        hduinc = fits.ImageHDU(inc)
+        hduem = fits.ImageHDU(em)
+        hdupa = fits.ImageHDU(pa)
+        listhdu = fits.HDUList([hdu,tbhdu,tbahdu,tbbhdu,hduinc,hduem,hdupa])
+        hdr = listhdu[0].header
+        hdr += head
+        thdr = listhdu[1].header
+        tahdr = listhdu[2].header
+        tbhdr = listhdu[3].header
+        thdr.set('extname', 'WCS-TAB')
+        tahdr.set('extname', 'WCS-CTAB')
+        tbhdr.set('extname', 'WCS-WTAB')
+        thdr.set('TSCAL1', factor)
+        tahdr.set('TSCAL1', factor)
+        tbhdr.set('TSCAL1', factor)
+
+        listhdu[4].header.set('extname', 'INCIDENCE')
+        listhdu[4].header.set('BSCALE', factor)
+        listhdu[5].header.set('extname', 'EMERGENCE')
+        listhdu[5].header.set('BSCALE', factor)
+        listhdu[6].header.set('extname', 'PHASE')
+        listhdu[6].header.set('BSCALE', factor)
+
         hdr['TELESCOP'] = telescop
         hdr['AUTHOR'] = author
         hdr['OBJECT'] = target
         hdr['INSTRUME'] = instrume
-        try:
-          listfits.writeto(myfits)
-        except:
-          hdu.writeto(myfits)
+
+        listhdu.writeto(myfits)
 
       else:
         print 'Cannot find image %s!\n' %(myimage)
